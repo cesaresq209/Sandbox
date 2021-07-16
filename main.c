0b1a195e413868b79b8664e60cb19312edbbab70
Cesar Rodriguez <cesar.rodriguezesqueda@resideo.com>
2021-07-15 18:21:02 -0600

    - Adding comments

--- main.c
+++ main.c
@@ -6,12 +6,15 @@ void main(){
 	Uart_Init();
 	while(1)
 	{
+		//Read GPIO
 		if(GPIO_Read(PORTA, Pin2) == 1)
 		{
+			// Set GPIO to High
 			GPIO_Write(PORTA, Pin3, 1);
 		}
 		else
 		{
+			// Set GPIO to Low
 			GPIO_Write(PORTA, Pin3, 0);
 		}
 	}
