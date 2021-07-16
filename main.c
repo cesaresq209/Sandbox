44cdc8a0f80f58419e8d11409a301f4cf83272a1
Cesar Rodriguez <cesar.rodriguezesqueda@resideo.com>
2021-07-15 18:18:08 -0600

    - Adding logic to read gpio and activate gpio

--- main.c
+++ main.c
@@ -6,6 +6,13 @@ void main(){
 	Uart_Init();
 	while(1)
 	{
-		//TODO
+		if(GPIO_Read(PORTA, Pin2) == 1)
+		{
+			GPIO_Write(PORTA, Pin3, 1);
+		}
+		else
+		{
+			GPIO_Write(PORTA, Pin3, 0);
+		}
 	}
 }
